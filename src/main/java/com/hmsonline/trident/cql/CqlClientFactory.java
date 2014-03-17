5089bcf70e4e729854c040e37f92e7ee711cb415
James Tyack <jtyack@healthmarketscience.com>
2014-03-17 16:47:14 -0400

    MF-1800 MF-1833 #time 3h #comment Extract cluster building code to lazy method that is called when connection is first used rather than on topol startup.

--- src/main/java/com/hmsonline/trident/cql/CqlClientFactory.java
+++ src/main/java/com/hmsonline/trident/cql/CqlClientFactory.java
@@ -19,29 +19,20 @@ public class CqlClientFactory implements Serializable {
     private static final Logger LOG = LoggerFactory.getLogger(CqlClientFactory.class);
     private Map<String, Session> sessions = new HashMap<String, Session>();
     private Session defaultSession = null;
+    private String[] hosts;
     protected static Cluster cluster;
 
     @SuppressWarnings("rawtypes")
     public CqlClientFactory(Map configuration) {
         String hostProperty = (String) configuration.get(CassandraCqlStateFactory.TRIDENT_CASSANDRA_CQL_HOSTS);
-        String[] hosts = hostProperty.split(",");
-        try {
-            if (LOG.isDebugEnabled()) {
-                for (String host : hosts) {
-                    LOG.debug("Connecting to [" + host + "]");
-                }
-            }
-            cluster = Cluster.builder().addContactPoints(hosts).build();
-        } catch (NoHostAvailableException e) {
-            throw new RuntimeException(e);
-        }
+        hosts = hostProperty.split(";");
     }
 
     public synchronized Session getSession(String keyspace) {
         Session session = sessions.get(keyspace);
         if (session == null) {
             LOG.debug("Constructing session for keyspace [" + keyspace + "]");
-            session = cluster.connect(keyspace);
+            session = getCluster().connect(keyspace);
             sessions.put(keyspace, session);
         }
         return session;
@@ -49,7 +40,26 @@ public class CqlClientFactory implements Serializable {
 
     public synchronized Session getSession() {
         if (defaultSession == null)
-            defaultSession = cluster.connect();
+            defaultSession = getCluster().connect();
         return defaultSession;
+    }
+    
+    public Cluster getCluster() {
+        if (cluster == null) {
+            try {
+                if (LOG.isDebugEnabled()) {
+                    for (String host : hosts) {
+                        LOG.debug("Connecting to [" + host + "]");
+                    }
+                }
+                cluster = Cluster.builder().addContactPoints(hosts).build();
+                if (cluster == null) {
+                    throw new RuntimeException("Could not connect to Cassandra cluster with hosts " + hosts);
+                }
+            } catch (NoHostAvailableException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return cluster;
     }
 }
