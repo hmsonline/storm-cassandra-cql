30eec69b24e4305e852fc6f29c7eb139ad2cde32
Isaac Rieksts <irieksts@hmsonline.com>
2014-07-28 11:42:13 -0400

    Adding port to the config for storm cassandra cql

--- src/main/java/com/hmsonline/trident/cql/CqlClientFactory.java
+++ src/main/java/com/hmsonline/trident/cql/CqlClientFactory.java
@@ -1,9 +1,13 @@
 package com.hmsonline.trident.cql;
 
 import java.io.Serializable;
+import java.net.InetSocketAddress;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,12 +24,17 @@ public class CqlClientFactory implements Serializable {
     private Map<String, Session> sessions = new HashMap<String, Session>();
     private Session defaultSession = null;
     private String[] hosts;
+    private String[] ports;
     protected static Cluster cluster;
 
     @SuppressWarnings("rawtypes")
     public CqlClientFactory(Map configuration) {
         String hostProperty = (String) configuration.get(CassandraCqlStateFactory.TRIDENT_CASSANDRA_CQL_HOSTS);
         hosts = hostProperty.split(",");
+        String portProperty = (String) configuration.get(CassandraCqlStateFactory.TRIDENT_CASSANDRA_CQL_PORTS);
+        if(StringUtils.isNotBlank(portProperty)) {
+            ports = portProperty.split(",");
+        }
     }
 
     public synchronized Session getSession(String keyspace) {
@@ -47,12 +56,28 @@ public class CqlClientFactory implements Serializable {
     public Cluster getCluster() {
         if (cluster == null) {
             try {
-                if (LOG.isDebugEnabled()) {
-                    for (String host : hosts) {
-                        LOG.debug("Connecting to [" + host + "]");
+                if(ports != null) {
+                List<InetSocketAddress> sockets = new ArrayList<InetSocketAddress>();                    
+                int i = 0;
+                for (String host : hosts) {
+                    int port;
+                    if(ports.length == 1) {
+                        port = Integer.valueOf(ports[0]);
+                    } else {
+                        port = Integer.valueOf(ports[0]);
                     }
+                    sockets.add(new InetSocketAddress(host, port));
+                    LOG.debug("Connecting to [" + host + "] with port [" + port + "]");
                 }
-                cluster = Cluster.builder().addContactPoints(hosts).build();
+                cluster = Cluster.builder().addContactPointsWithPorts(sockets).build();
+                } else {
+                    if(LOG.isDebugEnabled()) {
+                        for (String host : hosts) {
+                            LOG.debug("Connecting to [" + host + "]");
+                        }
+                    }
+                    cluster = Cluster.builder().addContactPoints(hosts).build();
+                }
                 if (cluster == null) {
                     throw new RuntimeException("Critical error: cluster is null after "
                             + "attempting to build with contact points (hosts) " + hosts);
