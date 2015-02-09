fd4eac494e9446442137861515ecc8a6d8e8b698
Brian O'Neill <bone@alumni.brown.edu>
2015-02-09 14:49:15 -0500

    Removed old Word Count example in favor of the WordCountWithSource example.

--- src/test/java/com/hmsonline/trident/cql/example/wordcount/WordCountTopology.java
+++ src/test/java/com/hmsonline/trident/cql/example/wordcount/WordCountTopology.java
@@ -22,42 +22,6 @@ public class WordCountTopology {
     private static final Logger LOG = LoggerFactory.getLogger(WordCountTopology.class);
 
     @SuppressWarnings("unchecked")
-    public static StormTopology buildWordCountTopology(LocalDRPC drpc) {
-        LOG.info("Building topology.");
-        TridentTopology topology = new TridentTopology();
-
-        FixedBatchSpout spout1 = new FixedBatchSpout(new Fields("sentence"), 3,
-                new Values("the cow jumped over the moon"),
-                new Values("the man went to the store and bought some candy"),
-                new Values("four score and seven years ago"),
-                new Values("how many apples can you eat"));
-        spout1.setCycle(true);
-
-        // Quick note on an interesting error I encountered regarding using Count() as an aggregator:
-        // Count utilizes an aggregator that updates values with a Long type. As you might notice, schema.cql
-        // stores the count as an integer datatype within the wordcounttable. To remedy this issue, ensure
-        // that your input fields datatypes are consistent with your persistent aggregation function. 
-        // IntegerCount is exactly the same as storm.trident.operation.builtin.Count but instead has the
-        // Integer as its field.
-        TridentState wordCounts =
-                topology.newStream("spout1", spout1)
-                        .each(new Fields("sentence"), new Split(), new Fields("word"))
-                        .groupBy(new Fields("word"))
-                        .persistentAggregate(CassandraCqlMapState.nonTransactional(new WordCountMapper()),
-                                new IntegerCount(), new Fields("count"))
-                        .parallelismHint(6);
-
-        topology.newDRPCStream("words", drpc)
-                .each(new Fields("args"), new Split(), new Fields("word"))
-                .groupBy(new Fields("word"))
-                .stateQuery(wordCounts, new Fields("word"), new MapGet(), new Fields("count"))
-                .each(new Fields("count"), new FilterNull())
-                .aggregate(new Fields("count"), new Sum(), new Fields("sum"));
-
-        return topology.build();
-    }
-
-    @SuppressWarnings("unchecked")
     public static StormTopology buildWordCountAndSourceTopology(LocalDRPC drpc) {
         LOG.info("Building topology.");
         TridentTopology topology = new TridentTopology();
@@ -98,7 +62,7 @@ public class WordCountTopology {
         LOG.info("Submitting topology.");
         cluster.submitTopology("cqlexample", configuration, buildWordCountAndSourceTopology(client));
         LOG.info("Topology submitted.");
-        Thread.sleep(100000);
+        Thread.sleep(10000);
         LOG.info("DRPC Query: Word Count [cat, dog, the, man]: {}", client.execute("words", "cat dog the man"));
         cluster.shutdown();
         client.shutdown();
